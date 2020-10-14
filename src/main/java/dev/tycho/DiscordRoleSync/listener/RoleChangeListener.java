7315d2d7a6cd3d04f21f0465f004dc986b2b469d
Mitchell Monahan <me@mitchellmonahan.com>
2020-10-13 23:42:59 -0400

    Update to 1.16.3/DiscordSRV 1.20.0

--- src/main/java/dev/tycho/DiscordRoleSync/listener/RoleChangeListener.java
+++ src/main/java/dev/tycho/DiscordRoleSync/listener/RoleChangeListener.java
@@ -4,14 +4,14 @@ import dev.tycho.DiscordRoleSync.DiscordRoleSync;
 import github.scarsz.discordsrv.DiscordSRV;
 import github.scarsz.discordsrv.dependencies.jda.api.events.GenericEvent;
 import github.scarsz.discordsrv.dependencies.jda.api.hooks.EventListener;
-import net.dv8tion.jda.api.events.guild.member.GuildMemberRoleAddEvent;
-import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import github.scarsz.discordsrv.dependencies.jda.api.events.guild.member.GuildMemberRoleAddEvent;
+import github.scarsz.discordsrv.dependencies.jda.api.hooks.ListenerAdapter;
 import org.bukkit.entity.Player;
 
 import javax.annotation.Nonnull;
 import java.util.UUID;
 
-public class RoleChangeListener extends ListenerAdapter implements EventListener {
+public class RoleChangeListener extends ListenerAdapter {
     private final DiscordRoleSync plugin;
 
     public RoleChangeListener(DiscordRoleSync plugin) {
@@ -23,17 +23,8 @@ public class RoleChangeListener extends ListenerAdapter implements EventListener
         plugin.getLogger().info("Guild member " + event.getMember().getUser().getName() + " got " +
                 "a role update - resyncing groups...");
         UUID playerId = DiscordSRV.getPlugin()
-                                .getAccountLinkManager()
-                                .getUuid(event.getMember().getId());
+                .getAccountLinkManager()
+                .getUuid(event.getMember().getId());
         plugin.syncRoles(playerId);
-    }
-
-    @Override
-    public void onEvent(@Nonnull GenericEvent genericEvent) {
-        // apparently required for EventListener implementations... also apparantly the guild member role add event
-        // never got called?
-        if (genericEvent instanceof GuildMemberRoleAddEvent) {
-            onGuildMemberRoleAdd((GuildMemberRoleAddEvent) genericEvent);
-        }
     }
 }
